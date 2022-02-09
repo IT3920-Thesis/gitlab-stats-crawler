73144c278850d16a40b169461e79c5c602d115f4
Fredrik F. Lindhagen <fredrfli@studentmediene.no>
2022-02-09 11:22:09 +0100

    Ensure gitlab_access_token is flagged as sensitive

--- terraform/staging/variables.tf
+++ terraform/staging/variables.tf
@@ -1,4 +1,5 @@
 variable "gitlab_access_token" {
   type = string
+  sensitive = true
   description = "Access token which grants API and read_repository access to repositories"
 }
--- terraform/template/variables.tf
+++ terraform/template/variables.tf
@@ -17,6 +17,7 @@ variable "image_tag" {
 
 variable "gitlab_access_token" {
   type = string
+  sensitive = true
   description = "Access token which grants API and read_repository access to repositories"
 }
 
